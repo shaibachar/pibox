e395e62e65911c2675295a4fbd6bdede4ca31fd0
shaibachar@gmail.com <shaibachar@gmail.com>
2021-07-15 17:32:06 +0300

    add

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 'use strict';
-
 const express = require('express');
+
+const Gpio = require('onoff').Gpio;
+const relay1 = new Gpio(12, 'high'); // IMPORTANT: Use 'high' if relay uses low level trigger
+const relay2 = new Gpio(16, 'high');
+const relay3 = new Gpio(20, 'high');
+const relay4 = new Gpio(21, 'high');
 
 // Constants
 const PORT = 8080;
@@ -9,6 +14,22 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
+  setTimeout(() => {
+    relay1.writeSync(0);
+    relay2.writeSync(0);
+    relay3.writeSync(0);
+    relay4.writeSync(0);
+    console.log('Relay should be on. Relay will turn off in 5 seconds.');
+
+    setTimeout(() => {
+      relay1.writeSync(1);
+      relay2.writeSync(1);
+      relay3.writeSync(1);
+      relay4.writeSync(1);
+      console.log('Relay should be off. ');
+    }, 5000);
+  }, 5000);
+
   res.send('Hello World');
 });
 
